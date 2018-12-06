8889db8916d4fa0d2aa4a05b16667c6665165a3f
Orlee Gillis <orlee.gillis@gmail.com>
2018-12-06 20:04:03 +0200

    removed extra button

---
+++ scripts/player-bar.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+  $('button#play-pause').click( function() {
+    player.playPause();
+    $(this).attr('playState', player.playState);
+    const totalTime = player.prettyTime(player.currentlyPlaying.duration);
+  });
+
+  $('button#previous').click( function() {
+    if (player.playState !== 'playing') { return false }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1;
+    if (currentSongIndex < 1) { return; }
+
+    const newSong = album.songs[prevSongIndex];
+    player.playPause(newSong);
+  });
+
+  $('button#next').click( function() {
+    if (player.playState !== 'playing') { return false }
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
+    if (nextSongIndex >= album.songs.length) { return; }
+
+    const nextSong = album.songs[nextSongIndex];
+    player.playPause(nextSong);
+  });
--- scripts/song-list.js
+++ scripts/song-list.js
@@ -9,11 +9,6 @@
       <span class="ion-play"></span>
       <span class="ion-pause"></span>
     </button>
-    <button>
-      <span class="song-number">${index - 1}</span>
-      <span class="ion-play"></span>
-      <span class="ion-pause"></span>
-    </button>
   </td>
 <td>${song.title}</td>
 <td>${song.duration}</td>
