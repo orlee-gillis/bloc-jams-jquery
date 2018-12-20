34d724cf75c6a278c24ca38f49e843f8ff725b42
Orlee Gillis <orlee.gillis@gmail.com>
2018-12-20 20:32:56 +0200

    trying to get the slider bar to work for both time and volume

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,7 +1,22 @@
 {
+  $('button#previous').on('click', function() {
+if (player.playState !== 'playing') { return; }
+
+
+ const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+const nextSongIndex = currentSongIndex - 1;
+if (nextSongIndex >= album.songs.length) { return; }
+
+ const nextSong = album.songs[nextSongIndex];
+ player.playPause(nextSong);
+     $('#time-control .total-time').text( totalTime );
+});
   $('button#play-pause').on('click', function() {
     player.playPause();
     $(this).attr('playState', player.playState);
+
+    const totalTime = player.prettyTime(player.currentlyPlaying.duration);
+    $('#time-control .total-time').text( totalTime );
   });
 
 
@@ -16,17 +31,26 @@
 
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+        $('#time-control .total-time').text( totalTime );
   });
 
-     $('button#previous').on('click', function() {
-  if (player.playState !== 'playing') { return; }
 
 
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-  const nextSongIndex = currentSongIndex - 1;
-   if (nextSongIndex >= album.songs.length) { return; }
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+   });
 
-    const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
-  });
+   $('#volume-control input').on('input', function (event) {
+     player.skipTo(event.target.value);
+    });
+
+
+  setInterval( () => {
+     if (player.playState !== 'playing') { return; }
+     const currentTime = player.getTime();
+const duration = player.getDuration();
+const percent = (currentTime / duration) * 100;
+$('#time-control .current-time').text( player.prettyTime(currentTime) );
+     $('#time-control input').val(percent);
+  }, 1000);
 }
